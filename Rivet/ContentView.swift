ea0d00a8d4cba6424a342881ef30c332a18b32be
Brain-of-Brian <brianmo3530+github@gmail.com>
2026-07-13 14:29:12 -0500

    Initial Commit

---
+++ Rivet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rivet
+//
+//  Created by 14 BGCCLoan Lbrary on 7/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rivet/RivetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RivetApp.swift
+//  Rivet
+//
+//  Created by 14 BGCCLoan Lbrary on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct RivetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
